d131f2def9b21bba38b707508100277eaa2c61a7
bvuongx <bvuong@gmail.com>
2013-07-24 11:36:48 -0700

    Add the ability to view a list of all questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.all
   end
 
   def new
@@ -9,20 +10,16 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(params[:question])
-  if @question.save
-    flash[:notice] = "Question has been created."
-    redirect_to @question
-  else
-    flash[:alert] = "Question has not been created."
-    render :action => "new"
+    if @question.save
+      flash[:notice] = "Question has been created."
+      redirect_to @question
+    else
+      flash[:alert] = "Question has not been created."
+      render :action => "new"
+    end
   end
-end
-
 
   def show
     @question = Question.find(params[:id])
   end
-
-
-
 end
---
+++ spec/support/factories/question_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :question do
+    name "Example question"
+  end
+end
