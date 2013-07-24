f4a07dc35b09a389e97b1114c1c1051b5847ba23
bvuongx <bvuong@gmail.com>
2013-07-24 14:13:31 -0700

    Implement delete functionality for questions

---
+++ spec/integration/deleting_questions_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature "Deleting questions" do
+  scenario "Deleting a question" do
+    Factory(:question, :name => "TextMate 2")
+    visit "/"
+    click_link "TextMate 2"
+    click_link "Delete Question"
+    page.should have_content("Question has been deleted.")
+    visit "/"
+    page.should_not have_content("TextMate 2")
+  end
+end
